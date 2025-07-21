7f2d2ac57526fcfa07527a2b9208639ddbc9dd4b
MagdinS <magdin.stoica@icloud.com>
2025-07-21 09:46:03 -0400

    Fill the deck of cards with cards objects

--- CardGame_Interactive/CardGameApp/CardDeck.cs
+++ CardGame_Interactive/CardGameApp/CardDeck.cs
@@ -1,16 +1,46 @@
+
 namespace CardGameApp;
 
 public class CardDeck
 {
 	private List<Card> _cardList;
 
+	private const byte MAX_SUIT_COUNT = 4;
+	private const byte MAX_CARD_VALUE = 13;
 	public CardDeck()
 	{
 		_cardList = new List<Card>();
-	}
 
+		//fill the deck of cards
+		CreateCards();
+	}
 	public int CardCount
 	{
 		get { return _cardList.Count; }
 	}
+
+	/// <summary>
+	/// Create the all the cards for each suit
+	/// </summary>
+	/// <exception cref="NotImplementedException"></exception>
+	private void CreateCards()
+	{
+		//repeat for each of the four suites
+		for(byte iSuit = 1; iSuit <= MAX_SUIT_COUNT; iSuit++) 
+		{
+			CardSuit suit = (CardSuit)iSuit;
+
+			//repeat for each value in a suit
+			for(byte value = 1; value <= MAX_CARD_VALUE; value++)
+			{
+				//create a card with the current value and suit
+				Card card = new Card(value, suit);
+
+				//add the card to the list
+				_cardList.Add(card);
+			}
+		}
+	}
+
+
 }
